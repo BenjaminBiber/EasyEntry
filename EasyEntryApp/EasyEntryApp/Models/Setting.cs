bbdac4d929ad7abf738802bc1f02b44c26a23229
BenjiIstAmKochen <ben.biber.strass@gmail.com>
2025-02-23 11:45:51 +0100

    Added Version 2 Gerber Files

--- EasyEntryApp/EasyEntryApp/Models/Setting.cs
+++ EasyEntryApp/EasyEntryApp/Models/Setting.cs
@@ -1,29 +1,39 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using Newtonsoft.Json;
-
 namespace doorOpener.Models;
 
 public class Setting
 {
     public static readonly string FileName = FileSystem.AppDataDirectory + "/settings.json";
-
+    
     public static void SaveSettingToJSON(bool status)
     {
-        // var thingToSave = new return_Setting();
-        // thingToSave.ShowSnackBar = status;
             var writeData = JsonConvert.SerializeObject(status, Formatting.Indented);
             File.WriteAllText(Setting.FileName, writeData);
     }
 
-    public static bool GetSettingFromJSON()
+    public static bool GetSettingFromJSON(ISnackbar snackbar = null)
     {
         var returnBool = true;
         if (File.Exists(Setting.FileName))
-        { 
-            var existingData = File.ReadAllText(Setting.FileName);
-            if (!string.IsNullOrEmpty(existingData))
+        {
+            try
             {
-                returnBool = JsonConvert.DeserializeObject<bool>(existingData);
+                var existingData = File.ReadAllText(Setting.FileName);
+                if (!string.IsNullOrEmpty(existingData))
+                {
+                    returnBool = JsonConvert.DeserializeObject<bool>(existingData);
+                }
             }
+            catch (Exception e)
+            {
+                if (snackbar != null)
+                {
+                    snackbar.Add($"Fehler beim auslesen der Einstellungen: {e}", Severity.Error);
+                }
+            }
+           
         }
 
         return returnBool;
